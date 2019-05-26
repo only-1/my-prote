e0628da269b8af33ede5aba6dcc98271cd185b8d
郑宏振 <1456421562@qq.com>
2019-05-26 22:15:37 +0800

    list

--- my-prote/src/pages/city/City.vue
+++ my-prote/src/pages/city/City.vue
@@ -2,16 +2,22 @@
 <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alper></city-alper>
 </div>
 </template>
 <script>
 import CityHeader from "./components/Header.vue"
 import CitySearch from "./components/Search.vue"
+import CityList from "./components/List.vue"
+import CityAlper from "./components/Alper"
 export default {
     name:'city',
     components:{
        CityHeader,
-       CitySearch
+       CitySearch,
+       CityList,
+      CityAlper
 
     }
 }
---
+++ my-prote/src/pages/city/components/Alper.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul class="list">
+        <li class="item">a</li>
+        <li class="item">a</li>
+        <li class="item">a</li>
+        <li class="item">a</li>
+        <li class="item">a</li>
+    </ul>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="stylus" scoped>
+    .list
+        display:flex
+        flex-direction:column
+        position:absolute
+        top:1.58rem
+        right:0
+        bottom:0
+        width:.4rem
+        justify-content: center;
+        .item
+            text-align:center
+            line-height:.42rem
+</style>
